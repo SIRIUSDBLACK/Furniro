6d4ebeb284e5b59e2f7bfb94512b40df9122adb0
SIRIUSDBLACK <tzaw3720@gmail.com>
2024-09-04 18:24:13 +0630

    added products section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     extend: {
       colors: {
         favorite: '#FFF3E3', 
-        secondary: '#B88E2F'
+        secondary: '#B88E2F',
+        third: '#a5812c',
+        cardbg:'#F4F5F7',
+        cardtt: '#898989',
+        cardlt: '#B0B0B0', 
+        cardh: '#3A3A3A',
+        bodybg: '#F5F5F5',    
       },
     },
   },
